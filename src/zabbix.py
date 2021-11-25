e5fd4ef57703fb78ac98fe9896b9e682f057bf88
Daniel Isaksen <code@duniel.no>
2021-11-25 19:40:53 +0100

    zabbix.py: clean up and add comments

--- src/zabbix.py
+++ src/zabbix.py
@@ -5,17 +5,15 @@
 # Author:  Daniel Isaksen <daniel.isaksen@banenor.no>
 #
 
-from typing import Union, Optional
-
 import asyncio
 import aiohttp
-import json
 
 class APIException(Exception):
     def __init__(self, *args, **kwargs):
         super().__init__(*args)
 
-        self.error = kwargs.get("error", None)
+        # Grab the error code. TODO: Grab more information?
+        self.code = kwargs.get("code", None)
 
 class Zabbix(object):
     def __init__(self,
@@ -33,6 +31,7 @@ class Zabbix(object):
         # `ClientSession` has to be instantiated inside a coroutine.
         self._session = aiohttp.ClientSession()
 
+        # Set required headers up front.
         self._session.headers.update({
             "Content-Type":  "application/json-rpc",
             "User-Agent":    "Pano/0.0.1",
@@ -40,62 +39,75 @@ class Zabbix(object):
         })
 
     async def _aio_shutdown(self):
+        # Cleanly shut down the aiohttp session or else it complains. Loudly.
         await self._session.close()
 
     async def _login(self):
+        # Call the API method `user.login`.
         response = await self.user.login(
             user      = self._username,
             password  = self._password,
             _is_login = True
         )
 
+        # Oops.
         if "error" in response:
             raise APIException(
                 response["error"]["message"],
-                error = response["error"]["code"]
+                code = response["error"]["code"]
             )
 
+        # Extract the token itself.
         self._token = response.get("result")
 
     async def do(self,
                  method:    str,
                  params:    dict,
-                 login: bool = False):
-        # No pre-established session, perform login.
-        if not login and not self._token:
+                 _is_login: bool = False):
+        # No pre-established session, perform login. The `not login` part is to
+        # avoid calling `_login` again before authentication has finished.
+        if not _is_login and not self._token:
             await self._login()
 
+        # TODO: Don't send an API token for certain calls like `apiinfo.version`
+        # and `user.checkAuthentication`.
+
+        # Some of these fields are required by the Zabbix API.
         request_params = {
             "jsonrpc": "2.0",
-            "id":      1,
-            "auth":    self._token,
+            "id":      "_",         # Arbitrary identifier.
+            "auth":    self._token, # Authentication token
             "method":  method,
             "params":  params
         }
 
+        # Make the underlying HTTP call.
         response_obj = await self._session.post(
             url  = self._url,
             json = request_params
         )
 
+        # Extract JSON from the response.
         response = await response_obj.json()
 
+        # Handle errors.
         if "error" in response:
+            # The following code means we are not logged in.
             if response["error"].get("code", 0) == "-32602":
-                # Not logged in - perform login.
-                pass#await self._login()
+                await self._login()
             else:
+                # TODO: Format the exception message in a clearer way?
                 raise APIException(
                     response["error"]["message"],
-                    error = response["error"]["code"]
+                    code = response["error"]["code"]
                 )
 
-        # TODO: Extract actual data / error?
         return response
 
+    # If you call an instance of this class to execute an API method, say
+    # `zbx.host.get`, this function is called as `zbx.__getattr__("host")`.
     def __getattr__(self, first_part: str):
-        # With the example API method "host.get", this function handes the
-        # "host" part, then returns a wrapper class that handles "get".
+        # Return a wrapper class that handles the `get` part.
         return _MethodWrapper(first_part, self)
 
 class _MethodWrapper(object):
@@ -103,20 +115,23 @@ class _MethodWrapper(object):
         self._first_part = first_part
         self._parent = parent
 
+    # Similar to `Zabbix.__call__("host")`, this function is called as
+    # `_MethodWrapper.__getattr__("get")`
     def __getattr__(self, second_part: str):
         # Return a function that handles the method parameters.
         def fn(**kwargs):
             # Default to `False`,
-            login = False
+            _is_login = False
 
             # Extract the '_is_login' parameter for the method 'user.login'.
             if self._first_part == "user" and second_part == "login":
-                login = kwargs.pop("_is_login", False)
+                _is_login = kwargs.pop("_is_login", False)
 
+            # Call the parent's `do` function to do the actual work.
             return self._parent.do(
                 method = f"{self._first_part}.{second_part}",
                 params = kwargs,
-                login  = login
+                login  = _is_login
             )
 
         return fn
